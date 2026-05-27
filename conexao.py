f696faa918e1303094dd2936f8928769acc85328
Mariana Silva <maryevang.mx@gmail.com>
2026-05-27 17:40:38 -0300

    Resolvendo conexao.py

--- conexao.py
+++ conexao.py
@@ -4,12 +4,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 def conectar():
     conexao = mysql.connector.connect(
         host=os.getenv("localhost"),
         user=os.getenv("root"),
-        password=os.getenv("2510"),
-        database=os.getenv("vidapet")
+        password=os.getenv("suasenha"),
+        database=os.getenv("vidapet"),
     )
 
     return conexao
